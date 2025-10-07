0acd771a33e8893095ee993ed0bd5d14ba8113cc
ASC25-VISHRANTHI-GIT-HUB <avishranthi2304@gmail.com>
2025-10-07 12:57:48 +0530

    updated

--- lab-exp/lab2/large_number.py
+++ lab-exp/lab2/large_number.py
@@ -1,8 +1,8 @@
 def largest_number(a, b, c):
     return max(a, b, c)
 
-# Example usage
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
+
 print("The largest number is:", largest_number(num1, num2, num3))
